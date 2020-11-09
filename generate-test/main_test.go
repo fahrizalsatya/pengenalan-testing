22f0cc6af1ca933d684bb53fb18f31142c0a3eef
fahrizalsatya <fahrizalsatya.laksamana@gmail.com>
2020-11-09 14:43:12 +0700

    Generate unit tests for function

--- generate-test/main_test.go
+++ generate-test/main_test.go
@@ -2,15 +2,22 @@ package main
 
 import "testing"
 
-func Test_main(t *testing.T) {
+func TestAvg(t *testing.T) {
+	type args struct {
+		nos []int
+	}
 	tests := []struct {
 		name string
+		args args
+		want int
 	}{
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			main()
+			if got := Avg(tt.args.nos...); got != tt.want {
+				t.Errorf("Avg() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
